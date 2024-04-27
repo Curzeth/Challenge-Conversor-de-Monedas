810419b8bfc3c7eba2f41df628f45d07905ddb74
Curzeth <curzeth@hotmail.com>
2024-04-27 07:18:07 -0600

    getting sleepy

--- src/Menu.java
+++ src/Menu.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class Menu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("""
-                Bienvenido/a al Conversor de Monedas.
+                » Bienvenido/a al Conversor de Monedas «
                 -----------------------------------------------------
                 Ingresa el número según la opción deseada:
+                -----------------------------------------------------
                 1 - Peso mexicano → Dólar estadounidense
                 2 - Dólar estadounidense → Peso mexicano
                 3 - Peso colombiano → Dólar estadounidense
@@ -16,8 +18,37 @@ public class Menu {
                 7 - Salir
                 -----------------------------------------------------
                 """);
-        var valorIngresado = sc.nextLine();
-        System.out.println("Ingresa el valor que deseas convertir: ");
-        valorIngresado = sc.nextLine();
+        int opcion = sc.nextInt();
+        double monto;
+
+        switch (opcion) {
+            case 1:
+                String mxnToUsd = Solicitudes.getmxnToUsd();
+                break;
+            case 2:
+                String usdToMxn = Solicitudes.getustToMxn();
+                break;
+            case 3:
+                String copToUsd = Solicitudes.getustToMxn();
+                break;
+            case 4:
+                String usdToCop = Solicitudes.getustToMxn();
+                break;
+            case 5:
+                String arsToUsd = Solicitudes.getustToMxn();
+                break;
+            case 6:
+                String usdToArs = Solicitudes.getustToMxn();
+                break;
+            case 7:
+                System.out.println();
+                break;
+            default:
+                System.out.println();
+        }
+
+
+//        Ingrese el valor que desea convertir:
+
     }
 }
--- src/Solicitudes.java
+++ src/Solicitudes.java
@@ -3,13 +3,55 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class Solicitudes {
+    private String mxnToUsd = "https://v6.exchangerate-api.com/v6/0151594d2b78d98ebcb7b21f/pair/MXN/USD/AMOUNT";
+    private String usdToMxn = "https://v6.exchangerate-api.com/v6/0151594d2b78d98ebcb7b21f/pair/USD/MXN/AMOUNT";
+    private String copToUsd = "https://v6.exchangerate-api.com/v6/0151594d2b78d98ebcb7b21f/pair/COP/USD/AMOUNT";
+    private String usdToCop = "https://v6.exchangerate-api.com/v6/0151594d2b78d98ebcb7b21f/pair/USD/COP/AMOUNT";
+    private String arsToUsd = "https://v6.exchangerate-api.com/v6/0151594d2b78d98ebcb7b21f/pair/ARS/USD/AMOUNT";
+    private String usdToArs = "https://v6.exchangerate-api.com/v6/0151594d2b78d98ebcb7b21f/pair/USD/ARS/AMOUNT";
+
+    private Solicitudes() {}
+
+    public String getMxnToUsd() {
+        return mxnToUsd;
+    }
+
+    public String getUsdToMxn() {
+        return usdToMxn;
+    }
+
+    public String getCopToUsd() {
+        return copToUsd;
+    }
+
+    public String getUsdToCop() {
+        return usdToCop;
+    }
+
+    public String getArsToUsd() {
+        return arsToUsd;
+    }
+
+    public String getUsdToArs() {
+        return usdToArs;
+    }
+
     public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner valor = new Scanner(System.in);
+
+        var convertirValor = valor.nextLine();
+
+        public String urlCompleta(String urlBase + monto) {
+            return urlBase + monto;
+        }
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://v6.exchangerate-api.com/v6/0151594d2b78d98ebcb7b21f/latest/USD"))
+                .uri(URI.create())
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
